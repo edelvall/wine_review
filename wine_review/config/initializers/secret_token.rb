627b64852ef4acd8473e03dbe25392019736050e
eric_delvalle <edelvall@gmu.edu>
2014-03-25 21:54:02 -0400

    First commit?

---
+++ wine_review/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WineReview::Application.config.secret_key_base = 'ffd46898c320a8942f3c5edfe21939c3bebd5da1d414918ac84842b7f46752f9c7b016ae00f74fd886eb140617fac82e5a45a08fc747dca3e7f5be733e09c0f4'
