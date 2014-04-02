066564eb10e8ac9d5cadad82158c56c2e015c7f0
Eric D <ericdelv@gmail.com>
2014-04-01 21:35:37 -0400

    Final changes to Wine HW 9

--- wine_review/app/controllers/wines_controller.rb
+++ wine_review/app/controllers/wines_controller.rb
@@ -1,9 +1,44 @@
 class WinesController < ApplicationController
+  before_action :set_wine, only: [ :show, :edit, :update, :destroy ]
+
+  def new
+  	@wine = Wine.new
+  end
+
   def index
     @wines = Wine.all
   end
 
   def show
+  end
+
+  def create
+  	@wine = Wine.new(wine_params)
+  	@wine.save
+  	redirect_to @wine
+  end
+
+  def edit
+  end
+
+  def update
+    @wine.update(wine_params)
+    redirect_to @wine
+  end
+
+  def destroy
+    @wine.destroy
+    redirect_to wines_path
+  end
+
+private
+
+  def wine_params
+  	params.require(:wine).permit(:name, :winery, :varietal, :country, :year)
+  end
+
+  def set_wine
     @wine = Wine.find(params[:id])
   end
+
 end
