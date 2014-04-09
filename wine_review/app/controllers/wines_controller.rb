371fd3a056ecb2018ea910707c30ffc1018104ae
Eric DelValle <ericdelv@gmail.com>
2014-04-08 22:06:38 -0400

    Modified files for HW10

--- wine_review/app/controllers/wines_controller.rb
+++ wine_review/app/controllers/wines_controller.rb
@@ -6,7 +6,7 @@ class WinesController < ApplicationController
   end
 
   def index
-    @wines = Wine.all
+    @wines = Wine.order(:name).page(params[:page])
   end
 
   def show
@@ -14,16 +14,22 @@ class WinesController < ApplicationController
 
   def create
   	@wine = Wine.new(wine_params)
-  	@wine.save
-  	redirect_to @wine
+  	if @wine.save
+      redirect_to @wine, notice: "#{@wine.name} was created!"
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
-    @wine.update(wine_params)
-    redirect_to @wine
+    if @wine.update(wine_params)
+      redirect_to @wine, notice: "#{@wine.name} was created!"
+    else
+      render :new
+    end
   end
 
   def destroy
--- wine_review/app/models/wine.rb
+++ wine_review/app/models/wine.rb
@@ -1,2 +1,17 @@
 class Wine < ActiveRecord::Base
+
+scope :aged, -> { where('year < 1999') }
+scope :by, ->(name) { where('name = ?', name) }
+
+VARIETALS = ["merlot", "chianti", "malbec", 
+	"cabernet sauvignon", "pinot noir", "zinfandel",
+	"chardonnay", "sauvignon blanc", "pinot grigio"]
+
+validates :name, :year, :country, presence: true
+validates :year, numericality: { only_integer: true },
+   unless: "year.blank?"
+validates :varietal, :inclusion => { :in => VARIETALS }   	
+
+  #must have a constant VARIETALS [merlot, chianti, etc...]
+  #varietal is valid from list (hint: inclusion)
 end
